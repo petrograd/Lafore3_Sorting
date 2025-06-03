1083594101840bd77541cb953c249219e525303f
Petr <pmerkuryev@gmail.com>
2025-06-03 13:07:54 +0200

    Initial commit

---
+++ BubbleSort/src/BubbleSortApp.java
@@ -0,0 +1,15 @@
+class ArrayBub {
+    private long[] a;
+    private int nElems;
+
+    public ArrayBub(int max) {
+        this.a = new long[max];
+        nElems = 0;
+    }
+
+}
+public class BubbleSortApp {
+    public static void main(String[] args) {
+
+    }
+}
